4c87c6866750a4c437632f4b841162058a61b8e2
Mark Anderson <mark@i-lateral.com>
2016-11-25 13:12:52 +0000

    Update ChildHubpage_Controller.php

--- code/control/ChildHubpage_Controller.php
+++ code/control/ChildHubpage_Controller.php
@@ -2,5 +2,12 @@
 
 class ChildHubPage_Controller extends Page_Controller
 {
+    public function PaginatedChildren($limit = 9) {
+        $list = $this->Children();
 
+        $pages =  new PaginatedList($list, $this->getRequest());
+        $pages->setpageLength($limit);
+
+        return $pages;
+    }
 }
